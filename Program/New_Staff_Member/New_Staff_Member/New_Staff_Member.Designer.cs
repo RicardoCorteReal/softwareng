f63338e1a73de8e0d216dd92beda7146fb3a46af
Niall Moss <32483123+NiallMoss@users.noreply.github.com>
2017-10-24 13:06:02 +0100

    Add files via upload

---
+++ Program/New_Staff_Member/New_Staff_Member/New_Staff_Member.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace New_Staff_Member
+{
+    partial class New_Staff_Member
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelFirstName = new System.Windows.Forms.Label();
+            this.ButtonHome = new System.Windows.Forms.Button();
+            this.ButtonSubmit = new System.Windows.Forms.Button();
+            this.ButtonCancel = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.DateTimeDob = new System.Windows.Forms.DateTimePicker();
+            this.LabelSurname = new System.Windows.Forms.Label();
+            this.LabelAddress1 = new System.Windows.Forms.Label();
+            this.LabelAddress2 = new System.Windows.Forms.Label();
+            this.LabelCounty = new System.Windows.Forms.Label();
+            this.LabelPostCode = new System.Windows.Forms.Label();
+            this.LabelJobPosition = new System.Windows.Forms.Label();
+            this.LabelDob = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LabelFirstName
+            // 
+            this.LabelFirstName.AutoSize = true;
+            this.LabelFirstName.Location = new System.Drawing.Point(12, 55);
+            this.LabelFirstName.Name = "LabelFirstName";
+            this.LabelFirstName.Size = new System.Drawing.Size(57, 13);
+            this.LabelFirstName.TabIndex = 0;
+            this.LabelFirstName.Text = "First Name";
+            // 
+            // ButtonHome
+            // 
+            this.ButtonHome.Location = new System.Drawing.Point(13, 13);
+            this.ButtonHome.Name = "ButtonHome";
+            this.ButtonHome.Size = new System.Drawing.Size(75, 23);
+            this.ButtonHome.TabIndex = 1;
+            this.ButtonHome.Text = "Home";
+            this.ButtonHome.UseVisualStyleBackColor = true;
+            // 
+            // ButtonSubmit
+            // 
+            this.ButtonSubmit.Location = new System.Drawing.Point(223, 498);
+            this.ButtonSubmit.Name = "ButtonSubmit";
+            this.ButtonSubmit.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSubmit.TabIndex = 2;
+            this.ButtonSubmit.Text = "Submit";
+            this.ButtonSubmit.UseVisualStyleBackColor = true;
+            // 
+            // ButtonCancel
+            // 
+            this.ButtonCancel.Location = new System.Drawing.Point(223, 527);
+            this.ButtonCancel.Name = "ButtonCancel";
+            this.ButtonCancel.Size = new System.Drawing.Size(75, 23);
+            this.ButtonCancel.TabIndex = 3;
+            this.ButtonCancel.Text = "Cancel";
+            this.ButtonCancel.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 7;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.Controls.Add(this.label6, 6, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 4, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(13, 339);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(523, 86);
+            this.tableLayoutPanel1.TabIndex = 4;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(447, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Sunday";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(373, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Saturday";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Tuesday";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Monday";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Wednesday";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(225, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Thursday";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(299, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Friday";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(155, 72);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 163);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(155, 163);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(155, 115);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 10;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(17, 214);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(98, 69);
+            this.listBox1.TabIndex = 11;
+            // 
+            // DateTimeDob
+            // 
+            this.DateTimeDob.Location = new System.Drawing.Point(155, 214);
+            this.DateTimeDob.Name = "DateTimeDob";
+            this.DateTimeDob.Size = new System.Drawing.Size(200, 20);
+            this.DateTimeDob.TabIndex = 12;
+            // 
+            // LabelSurname
+            // 
+            this.LabelSurname.AutoSize = true;
+            this.LabelSurname.Location = new System.Drawing.Point(152, 55);
+            this.LabelSurname.Name = "LabelSurname";
+            this.LabelSurname.Size = new System.Drawing.Size(49, 13);
+            this.LabelSurname.TabIndex = 13;
+            this.LabelSurname.Text = "Surname";
+            // 
+            // LabelAddress1
+            // 
+            this.LabelAddress1.AutoSize = true;
+            this.LabelAddress1.Location = new System.Drawing.Point(12, 99);
+            this.LabelAddress1.Name = "LabelAddress1";
+            this.LabelAddress1.Size = new System.Drawing.Size(77, 13);
+            this.LabelAddress1.TabIndex = 14;
+            this.LabelAddress1.Text = "Address Line 1";
+            // 
+            // LabelAddress2
+            // 
+            this.LabelAddress2.AutoSize = true;
+            this.LabelAddress2.Location = new System.Drawing.Point(152, 99);
+            this.LabelAddress2.Name = "LabelAddress2";
+            this.LabelAddress2.Size = new System.Drawing.Size(77, 13);
+            this.LabelAddress2.TabIndex = 15;
+            this.LabelAddress2.Text = "Address Line 2";
+            // 
+            // LabelCounty
+            // 
+            this.LabelCounty.AutoSize = true;
+            this.LabelCounty.Location = new System.Drawing.Point(12, 147);
+            this.LabelCounty.Name = "LabelCounty";
+            this.LabelCounty.Size = new System.Drawing.Size(40, 13);
+            this.LabelCounty.TabIndex = 16;
+            this.LabelCounty.Text = "County";
+            // 
+            // LabelPostCode
+            // 
+            this.LabelPostCode.AutoSize = true;
+            this.LabelPostCode.Location = new System.Drawing.Point(152, 147);
+            this.LabelPostCode.Name = "LabelPostCode";
+            this.LabelPostCode.Size = new System.Drawing.Size(56, 13);
+            this.LabelPostCode.TabIndex = 17;
+            this.LabelPostCode.Text = "Post Code";
+            // 
+            // LabelJobPosition
+            // 
+            this.LabelJobPosition.AutoSize = true;
+            this.LabelJobPosition.Location = new System.Drawing.Point(14, 198);
+            this.LabelJobPosition.Name = "LabelJobPosition";
+            this.LabelJobPosition.Size = new System.Drawing.Size(64, 13);
+            this.LabelJobPosition.TabIndex = 18;
+            this.LabelJobPosition.Text = "Job Position";
+            // 
+            // LabelDob
+            // 
+            this.LabelDob.AutoSize = true;
+            this.LabelDob.Location = new System.Drawing.Point(152, 198);
+            this.LabelDob.Name = "LabelDob";
+            this.LabelDob.Size = new System.Drawing.Size(66, 13);
+            this.LabelDob.TabIndex = 19;
+            this.LabelDob.Text = "Date of Birth";
+            // 
+            // New_Staff_Member
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 562);
+            this.Controls.Add(this.LabelDob);
+            this.Controls.Add(this.LabelJobPosition);
+            this.Controls.Add(this.LabelPostCode);
+            this.Controls.Add(this.LabelCounty);
+            this.Controls.Add(this.LabelAddress2);
+            this.Controls.Add(this.LabelAddress1);
+            this.Controls.Add(this.LabelSurname);
+            this.Controls.Add(this.DateTimeDob);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.ButtonCancel);
+            this.Controls.Add(this.ButtonSubmit);
+            this.Controls.Add(this.ButtonHome);
+            this.Controls.Add(this.LabelFirstName);
+            this.Name = "New_Staff_Member";
+            this.Text = "New Staff Member";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelFirstName;
+        private System.Windows.Forms.Button ButtonHome;
+        private System.Windows.Forms.Button ButtonSubmit;
+        private System.Windows.Forms.Button ButtonCancel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DateTimePicker DateTimeDob;
+        private System.Windows.Forms.Label LabelSurname;
+        private System.Windows.Forms.Label LabelAddress1;
+        private System.Windows.Forms.Label LabelAddress2;
+        private System.Windows.Forms.Label LabelCounty;
+        private System.Windows.Forms.Label LabelPostCode;
+        private System.Windows.Forms.Label LabelJobPosition;
+        private System.Windows.Forms.Label LabelDob;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Program/New_Staff_Member/New_Staff_Member/New_Staff_Member.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace New_Staff_Member
+{
+    public partial class New_Staff_Member : Form
+    {
+        public New_Staff_Member()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
